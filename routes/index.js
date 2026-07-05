aa67b7c3821a0401de3d701a9538f804614fcd1c
Benjamin265-sm <benjaminmbaisa@gmail.com>
2026-07-05 23:18:19 +0200

    change

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.tiongeRoute);
-routes.get('/benjamin', lesson1Controller.benjaminRoute);
-routes.get('/ekari', lesson1Controller.ekariRoute);
-
-module.exports = routes;
