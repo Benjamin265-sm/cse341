ca28e5d5ee0a209db25aa7b4763e9546c576de0f
Benjamin265-sm <benjaminmbaisa@gmail.com>
2026-07-07 03:33:47 +0200

    users

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ const getSingle = (req, res) => {
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId})
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(users[0]);
 
     });
 };
