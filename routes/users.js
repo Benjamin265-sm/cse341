ca63ffd813ef46abeecd2da455faa0669ca880d5
Benjamin265-sm <benjaminmbaisa@gmail.com>
2026-07-07 03:31:44 +0200

    project1

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users');
+
+router.get('/users', usersController.getAll);
+
+router.get('/users/:id', usersController.getSingle);
+
+module.exports = router;
